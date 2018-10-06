627a5dbfab1c1c70af748f6f6b9ca3cdbcc0fa82
Hernán Cote <hernan.cote@outlook.com>
2018-10-06 15:38:52 -0500

    Added getCourse function

--- contracts/Certifier.sol
+++ contracts/Certifier.sol
@@ -5,12 +5,48 @@ contract Certifier {
     address public owner;
     uint public last_completed_migration;
 
+    mapping(bytes32 => Course) courses;
+
+    struct Course { 
+        string name;
+        uint cost; 
+        uint duration; 
+        uint threshold;
+        bytes32[] codes;
+        mapping(address => Student) students;
+    }
+
+    struct Student {
+        string firstName;
+        string lastName;
+        uint age;
+        string email;
+        bytes32[] codes; 
+    }
+
     constructor() public {
         owner = msg.sender;
     }
 
     modifier restricted() {
-        if (msg.sender == owner) _;
+        require (msg.sender == owner); 
+        _;
     }
 
+    function addCourse(bytes32 _code,  string _name,  uint _cost,  uint _duration, uint _threshold, bytes32[] _codes) public restricted {
+        courses[_code] = Course({
+            name: _name,
+            cost: _cost,
+            duration: _duration,
+            threshold: _threshold,
+            codes: _codes 
+        });
+    }
+
+    function getCourse(bytes32 _code) public restricted view returns(string, uint, uint, uint, bytes32[]) {
+        Course storage course = courses[_code];
+        return(course.name, course.cost, course.duration, course.threshold, course.codes);
+    }
+
+    //TODO: Student must enrolle
 }
--- test/TestCertifier.sol
+++ test/TestCertifier.sol
@@ -10,4 +10,18 @@ contract TestCertifier {
         Certifier certifier = Certifier(DeployedAddresses.Certifier());
         Assert.equal(certifier.owner(), msg.sender, "Expected owner was not certified");
     }
+
+    function testAddCourse() public {
+        bytes32 code = "SC";
+        string storage name = "Solidity Course";
+        uint cost = 3;
+        uint duration = 9;
+        uint threshold = 80;
+        bytes32[] storage codes = ["abc", "bcd", "cde"];
+
+        Certifier certifier = Certifier(DeployedAddresses.Certifier());
+        
+        Assert.equal(certifier.addCourse(code, name, cost, duration, threshold, codes), 66, "Tx 66 char");
+        
+    }
 }
