b9a0fcd91cfd32bf4a738a40a77eb8b8db5da2f8
Hernán Cote <hernan.cote@outlook.com>
2018-10-06 13:38:31 -0500

    Inlitial commit

---
+++ test/TestCertifier.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.23;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Certifier.sol";
+
+contract TestCertifier {
+    
+    function testContractInstance() public {
+        Certifier certifier = Certifier(DeployedAddresses.Certifier());
+        Assert.equal(certifier.owner(), msg.sender, "Expected owner was not certified");
+    }
+}
